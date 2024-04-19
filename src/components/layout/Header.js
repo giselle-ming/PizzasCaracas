8d44cb879f24622971c71dba36263a3fa5288f3c
giselle <giselle.mingue@gmail.com>
2024-04-18 22:14:30 -0400

    adding file

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -10,14 +10,15 @@ export default function Header() {
         <nav className="flex gap-8 items-center text-gray-500 font-semibold">
           <Link href={""}>Inicio</Link>
           <Link href={""}>Menú</Link>
-          <Link href={""}>Crear Orden</Link>
           <Link href={""}>Contacto</Link>
-          {/* <Link
-            href={""}
+        </nav>
+        <nav className="flex items-center gap-4 text-gray-500 font-semibold">
+          <Link
+            href={"/login"}
             className="bg-primary rounded-full text-white px-4 py-2"
           >
             Ingresar
-          </Link> */}
+          </Link>
         </nav>
       </header>
     </>
